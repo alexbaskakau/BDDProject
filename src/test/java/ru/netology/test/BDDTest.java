9bbce0e29dc355fc61df5ff807dd2328c4acd751
Alex Baskakau <lenovos6000v4@gmail.com>
2023-06-08 12:52:22 +0100

    Lombok added

---
+++ src/test/java/ru/netology/test/BDDTest.java
@@ -0,0 +1,24 @@
+package ru.netology.test;
+
+
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.LoginPage;
+
+
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class BDDTest {
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCards() {
+        open("http://localhost:7777");
+        var loginData = DataHelper.getAuthInfo();
+
+        new LoginPage()
+                .validLogin(loginData)
+                .validVerify(DataHelper.getVerificationCodeFor(loginData));
+
+    }
+}
